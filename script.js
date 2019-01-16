52c0665c3849a6ff2c13e3138be1c278a213edd0
JocelynMayancela <44040580+JocelynMayancela@users.noreply.github.com>
2019-01-16 17:34:25 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,122 @@
+$("#p1").hide();
+$("#p2").hide();
+$("#p3").hide();
+$("#p4").hide();
+$("#p5").hide();
+$("#p6").hide();
+$("#p7").hide();
+$("#p8").hide();
+$("#p9").hide();
+
+$(".b1").click(function(){
+    $("#p1").show();
+    $("#p2").hide();
+    $("#p3").hide();
+    $("#p4").hide();
+    $("#p5").hide();
+    $("#p6").hide();
+    $("#p7").hide();
+    $("#p8").hide();
+    $("#p9").hide();
+});
+
+$(".b2").click(function(){
+     $("#p2").show();
+    $("#p1").hide();
+    $("#p3").hide();
+    $("#p4").hide();
+    $("#p5").hide();
+    $("#p6").hide();
+    $("#p7").hide();
+    $("#p8").hide();
+    $("#p9").hide();
+   });
+
+$(".b3").click(function(){
+  $("#p3").show();
+  $("#p1").hide();
+  $("#p2").hide();
+  $("#p4").hide();
+  $("#p4").hide();
+  $("#p5").hide();
+  $("#p6").hide();
+  $("#p7").hide();
+  $("#p8").hide();
+  $("#p9").hide();
+   });
+   $(".b4").click(function(){
+   $("#p4").show();
+   $("#p1").hide();
+   $("#p2").hide();
+   $("#p3").hide();
+   $("#p5").hide();
+   $("#p6").hide();
+   $("#p7").hide();
+   $("#p8").hide();
+   $("#p9").hide();
+       
+   });
+   $(".b5").click(function(){
+   $("#p5").show();
+   $("#p1").hide();
+   $("#p2").hide();
+   $("#p3").hide();
+   $("#p4").hide();
+   $("#p4").hide();
+   $("#p6").hide();
+   $("#p7").hide();
+   $("#p8").hide();
+   $("#p9").hide();
+    });
+    
+    $(".b6").click(function(){
+       $("#p6").show();
+       $("#p1").hide();
+       $("#p2").hide();
+       $("#p3").hide();
+       $("#p4").hide();
+       $("#p5").hide();
+       $("#p7").hide();
+       $("#p8").hide();
+       $("#p9").hide(); 
+    });
+    
+    $(".b7").click(function(){
+    $("#p7").show();
+    $("#p1").hide();
+    $("#p2").hide();
+    $("#p3").hide();
+    $("#p4").hide();
+    $("#p5").hide();
+    $("#p6").hide();
+    $("#p8").hide();
+    $("#p9").hide();
+});
+
+$(".b8").click(function(){
+    $("#p8").show();
+    $("#p1").hide();
+    $("#p2").hide();
+    $("#p3").hide();
+    $("#p4").hide();
+    $("#p5").hide();
+    $("#p6").hide();
+    $("#p7").hide();
+    $("#p9").hide();
+});
+
+$(".b9").click(function(){
+    $("#p9").show();
+    $("#p1").hide();
+    $("#p2").hide();
+    $("#p3").hide();
+    $("#p4").hide();
+    $("#p5").hide();
+    $("#p6").hide();
+    $("#p7").hide();
+    $("#p8").hide();
+   
+});
+
+
+
